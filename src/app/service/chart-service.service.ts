59b8ddba508dfea8697cf25da39a18ab06a9633f
alojoarboleda2003 <alonsoarboleda17@gmail.com>
2025-06-10 11:31:11 -0500

    Initial commit on angular_alonso

---
+++ src/app/service/chart-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+export interface EstadoOriginal {
+  estado: string;
+  cantidad: number;
+}
+
+export interface EstadoCantidad {
+  time: string;     // este es el "estado"
+  value: number;    // este es la "cantidad"
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartDataService {
+  private apiUrl = 'http://localhost/hermes_angular/getEstadoCounts.php';
+
+  constructor(private http: HttpClient) {}
+
+  getEstados(): Observable<EstadoCantidad[]> {
+    return this.http.get<EstadoOriginal[]>(this.apiUrl).pipe(
+      map((datos) =>
+        datos.map(item => ({
+          time: item.estado,   // mostramos el estado como texto
+          value: item.cantidad
+        }))
+      )
+    );
+  }
+}
