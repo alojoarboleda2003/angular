2c37b240f84c4a697d11aabae74386ac7e117160
alojoarboleda2003 <alonsoarboleda17@gmail.com>
2025-06-27 12:33:21 -0500

    haciendo diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './productos/productos.component';
+import { VenderComponent } from './vender/vender.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'productos', component: ProductosComponent },
+  { path: 'vender', component: VenderComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  // otras rutas aquí
+ // { path: '', redirectTo: '/productos', pathMatch: 'full' } // ruta por defecto
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/vender/vender.component.ts
+++ src/app/vender/vender.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class VenderComponent {
 
+  productos = [
+    { id: 1, nombre: 'Hamburguesa', precio: 30000 },
+    { id: 2, nombre: 'Perro Caliente', precio: 20000 },
+    { id: 3, nombre: 'Coca Cola', precio: 3000 }
+  ];
+
+  productoSeleccionado: any = null;
+  cantidad: number = 1;
+
+  ventaActual: { producto: any; cantidad: number }[] = [];
+
+  get totalVenta(): number {
+    return this.ventaActual.reduce((total, item) => total + (item.producto.precio * item.cantidad), 0);
+  }
+
+  agregarProducto() {
+    if (this.productoSeleccionado && this.cantidad > 0) {
+      const index = this.ventaActual.findIndex(item => item.producto.id === this.productoSeleccionado.id);
+      if (index > -1) {
+        // Si ya existe, suma la cantidad
+        this.ventaActual[index].cantidad += this.cantidad;
+      } else {
+        this.ventaActual.push({ producto: this.productoSeleccionado, cantidad: this.cantidad });
+      }
+      this.cantidad = 1;
+      this.productoSeleccionado = null;
+    }
+  }
+
+  eliminarProducto(index: number) {
+    this.ventaActual.splice(index, 1);
+  }
+
+  finalizarVenta() {
+    alert(`Venta finalizada. Total a pagar: ${this.totalVenta.toFixed(2)}`);
+    this.ventaActual = [];
+  }
 }
+
